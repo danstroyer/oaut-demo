a67f9359ec7790ff2d216457f169f16c31614182
Daniel <edfariasn@gmail.com>
2021-01-10 17:59:11 -0500

    Starting simple application on the OAUTH demo

---
+++ src/main/java/com/spark/oautdemo/OautDemoApplication.java
@@ -0,0 +1,13 @@
+package com.spark.oautdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OautDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OautDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spark/oautdemo/OautDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spark.oautdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OautDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
